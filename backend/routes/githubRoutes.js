7f5b39cb0890a8be75a937eba797c42c3aaf3722
NehaVishwakarma26 <nehasvish186@gmail.com>
2025-06-15 13:25:18 +0530

    middleware fix

--- backend/routes/githubRoutes.js
+++ backend/routes/githubRoutes.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const router=express.Router()
-const {githubLogin,githubCallback}=require("../controllers/githubController")
+const {githubLogin,githubCallback,logout}=require("../controllers/githubController")
+
+const requireAuth=require("../middlewares/authMiddleware")
 
 router.get("/login",githubLogin)
 router.get("/callback",githubCallback)
-
+router.post("/logout",requireAuth,logout)
 module.exports=router
