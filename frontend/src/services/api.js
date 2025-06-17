aeb38d5e93c9e807afe98e690cfa4333f5efd309
NehaVishwakarma26 <nehasvish186@gmail.com>
2025-06-17 14:01:41 +0530

    teams path fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -33,9 +33,9 @@ export const getHeatmapAndStreaks = () => API.get("/githubData/github/heatmap?si
 export const getSmartSuggestions=()=>API.get("/analytics/smart-suggestions")
 
 // ----------- Team ----------------------------
-export const getUserTeams=()=>API.get("/teams/my")
+export const getUserTeams=()=>API.get("/team/my")
 
-export const createTeam=(data)=>API.post("/teams/create",data)
+export const createTeam=(data)=>API.post("/team/create",data)
 
 
 export default API;
