d78cdf8d95f76877439156173eca1bc9c94d89d7
NehaVishwakarma26 <nehasvish186@gmail.com>
2025-06-24 17:06:32 +0530

    backend changes

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -8,11 +8,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-// ✅ Debug print
-console.log("🌥️ Cloudinary configured with:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY ? "✅ Present" : "❌ Missing",
-  api_secret: process.env.CLOUDINARY_API_SECRET ? "✅ Present" : "❌ Missing",
-});
+
 
 module.exports = cloudinary;
